0d317764d3d6fdd7c0d8037c2e5370df5c8f7304
AzIz000 <jbbtp00@gmail.com>
2015-07-18 20:19:45 +0200

    Crear pregunta

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,10 @@
 
 
 		router.get('/quizes', quizController.index);
+		router.get('/quizes/new', quizController.new);
 		router.get('/quizes/:quizId(\\d+)', quizController.show);
 		router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+		router.post('/quizes/create', quizController.create);
 
 
 
