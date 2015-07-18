2063420c9e4cdd0bfe9a24aec640e08cebadbf4d
AzIz000 <jbbtp00@gmail.com>
2015-07-18 18:18:49 +0200

    Lista Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,18 +10,18 @@ exports.index = function(req, res){
 
 //GET /quizes/:id
 exports.show = function(req,res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
 		res.render('quizes/show', {quiz: quiz});
 	})
 }
 
 //GET /quizes/:id/answer
 exports.answer = function(req,res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
 		if(req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer', {respuesta: 'Correcto'});
+			res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
 		}else{
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+			res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
 		}
 	})
 }
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@
 		  res.render('index', { title: 'Quiz' });
 		});
 
-		router.get('/quizes/question', quizController.question);
-		router.get('/quizes/answer', quizController.answer);
+		//router.get('/quizes/question', quizController.question);
+		//router.get('/quizes/answer', quizController.answer);
 		
 		router.get('/quizes', quizController.index);
 		router.get('/quizes/:quizId(\\d+)', quizController.show);
