89e68e696f8ccc5e34bef4435c8069a06b6e7562
AzIz000 <jbbtp00@gmail.com>
2015-07-18 19:42:19 +0200

    Busqueda preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,39 +1,55 @@
 var models = require('../models/models.js');
 
+//AUTOlOAD
+exports.load = function(req, res, next, quizId){
+	models.Quiz.findById(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			}else{
+				next(new Error('No existe quizId = ' + quizId));
+			}
+		}
+	).catch(function(error){next(error);});
+}
+
+
 
 //GET /quizes
 exports.index = function(req, res){
-	models.Quiz.findAll().then(function(quizes){
-			res.render('quizes/index.ejs', {quizes: quizes});
-	})
+	console.log(req.query.search);
+	if(req.query.search){
+		
+
+		models.Quiz.findAll({where: ["pregunta like ?", '%' + req.query.search.replace(/\s+/g, '%') + '%']}).then(
+			function(quizes){
+				res.render('quizes/index', {quizes: quizes});
+			}
+		).catch(function(error){next(error);});
+	}else{
+		models.Quiz.findAll().then(
+			function(quizes){
+				res.render('quizes/index', {quizes: quizes});
+			}
+		).catch(function(error){next(error);});
+	}
 }
 
 //GET /quizes/:id
 exports.show = function(req,res){
 	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: quiz});
-	})
+		res.render('quizes/show', {quiz: req.quiz});
+	});
 }
 
 //GET /quizes/:id/answer
 exports.answer = function(req,res){
 	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		if(req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
-		}else{
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
+		var resultado = 'Incorrecto';
+		if(req.query.respuesta === req.quiz.respuesta){
+			resultado = 'Correcto';
 		}
+			res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 	})
 }
-
-
-//GET /quizes/answer
-//exports.answer = function(req, res){
-//	models.Quiz.findAll().then(function(quiz){
-//		if(req.query.respuesta === quiz[0].respuesta){
-//			res.render('quizes/answer', {respuesta: 'Correcto'});
-//		}else{
-//			res.render('quizes/answer', {respuesta: 'Incorrecto'});
-//		}
-//	})
-//}
