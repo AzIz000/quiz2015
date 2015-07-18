d757d82232fa9f3a51b4d9e2c64b5050d8024de7
AzIz000 <jbbtp00@gmail.com>
2015-07-18 17:45:08 +0200

    Lista preguntas

--- models/models.js
+++ models/models.js
@@ -21,8 +21,9 @@ exports.Quiz = Quiz;
 sequelize.sync().then(function(){
 	Quiz.count().then(function(count){
 		if(count === 0){
-			Quiz.create({pregunta: 'Capital de Italia', respuesta: 'Roma'}).then(function(){
+			Quiz.create({pregunta: 'Capital de Italia', respuesta: 'Roma'});
+			Quiz.create({pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}).then(function(){
 					console.log('Base de datos inicializada')});
-				};
+			};
 	});
 });	
